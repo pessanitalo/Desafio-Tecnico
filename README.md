50f51e344b7479934ab472d0e06d40b87ea4a814
Italo Pessan <italopessan@gmail.com>
2026-03-13 11:15:53 -0300

    Estrutura inicial do projeto

--- README.md
+++ README.md
@@ -1,1 +1,1 @@
-# Desafio-Tecnico
+# desafio-tecnico
---
+++ desafio-tecnico.api/Program.cs
@@ -0,0 +1,36 @@
+using Desafio_Tecnico.Application.Interfaces;
+using Desafio_Tecnico.Application.Services;
+using Desafio_Tecnico.Data.Context;
+using Desafio_Tecnico.Data.Repository;
+using Microsoft.EntityFrameworkCore;
+
+var builder = WebApplication.CreateBuilder(args);
+
+// Add services to the container.
+
+builder.Services.AddControllers();
+// Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
+builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddSwaggerGen();
+
+builder.Services.AddDbContext<DataContext>(options =>
+    options.UseSqlServer(builder.Configuration.GetConnectionString("DefaultConnection")));
+
+builder.Services.AddScoped<IContatoRepository, ContatoRepository>();
+builder.Services.AddScoped<IContatoService, ContatoService>();
+
+var app = builder.Build();
+
+// Configure the HTTP request pipeline.
+if (app.Environment.IsDevelopment())
+{
+    app.UseSwagger();
+    app.UseSwaggerUI();
+}
+
+app.UseAuthorization();
+app.UseAuthorization();
+
+app.MapControllers();
+
+app.Run();
